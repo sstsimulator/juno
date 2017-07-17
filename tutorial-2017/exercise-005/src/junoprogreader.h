9e646936fd326c519e880682ec5f259a59c34703
Si Hammond <simon.hammond@gmail.com>
2017-07-16 18:18:01 -0600

    Ensure alignment for the data section

--- tutorial-2017/exercise-005/src/junoprogreader.h
+++ tutorial-2017/exercise-005/src/junoprogreader.h
@@ -16,50 +16,65 @@ namespace SST {
                 fread( (void*) &binVersion, sizeof(binVersion), 1, appBinary );
                 fread( (void*) &dataLen, sizeof(dataLen), 1, appBinary );
                 fread( (void*) &instLen, sizeof(instLen), 1, appBinary );
-                
+
+		padding = 0;
+
+		if( (dataLen + instLen) % 8 != 0 ) {
+			padding = 8 - ((dataLen + instLen) % 8);
+		}
+
                 output->verbose(CALL_INFO, 1, 0, "Application Version:      %" PRId64 "\n", binVersion);
                 output->verbose(CALL_INFO, 1, 0, "Data Length:              %" PRIu64 "\n", dataLen);
                 output->verbose(CALL_INFO, 1, 0, "Inst Length:              %" PRIu64 "\n", instLen);
-                
-                programBinary = (char*) malloc( sizeof(char) * (dataLen + instLen));
+		output->verbose(CALL_INFO, 1, 0, "Padding                   %" PRIu64 "\n", padding);
+
+                programBinary = (char*) malloc( sizeof(char) * (dataLen + instLen + padding));
+
+		for( uint64_t i = 0; i < (dataLen + instLen + padding); ++i ) {
+			programBinary[i] = 0;
+		}
+
                 fread( (void*) programBinary, sizeof(char), (dataLen + instLen), appBinary );
-                
+
                 for( uint64_t i = 0; i < dataLen; i += 8 ) {
                     int64_t tmp = 0;
                     memcpy( (void*) &tmp, (void*) &programBinary[i], sizeof(tmp) );
-                    
+
                     output->verbose(CALL_INFO, 2, 0, " -> %8" PRIu64 " : %" PRId64 "\n", i, tmp );
                 }
-                
+
             }
-            
+
             ~JunoProgramReader() {
                 free( programBinary );
             }
-            
+
             int64_t getBinaryVersion() {
                 return binVersion;
             }
-            
+
+	    uint64_t getPadding() {
+		return padding;
+	    }
+
             uint64_t getDataLength() {
                 return dataLen;
             }
-            
+
             uint64_t getInstLength() {
                 return instLen;
             }
-            
+
             char* getBinaryBuffer() {
                 return programBinary;
             }
-            
+
         protected:
             int64_t  binVersion;
             uint64_t dataLen;
             uint64_t instLen;
+	    uint64_t padding;
             char* programBinary;
-            
-            
         };
         
     }
